8ef7acfaae5a7f79c2752f2ec044e7696e60af62
xGentuso <ryan.mota@triosstudent.com>
2025-02-10 11:14:33 -0500

    Initial Commit

---
+++ ColleX/ColleXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColleXApp.swift
+//  ColleX
+//
+//  Created by ryan mota on 2025-02-10.
+//
+
+import SwiftUI
+
+@main
+struct ColleXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColleX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColleX
+//
+//  Created by ryan mota on 2025-02-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
